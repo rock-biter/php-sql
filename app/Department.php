47f70e124ee67f14be74c5b53763db7adbcede40
rock-biter <74297920+rock-biter@users.noreply.github.com>
2022-10-04 17:06:53 +0200

    add result to array method

--- app/Department.php
+++ app/Department.php
@@ -6,13 +6,59 @@ include_once __DIR__ . '/DB.php';
 
 class Department
 {
+  public $id;
+  public $name;
+  public $address;
+  public $phone;
+  public $email;
+  public $website;
+  public $head_of_department;
+
+  function __construct($id, $name, $address, $phone, $email, $website, $head_of_department)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->address = $address;
+    $this->phone = $phone;
+    $this->email = $email;
+    $this->website = $website;
+    $this->head_of_department = $head_of_department;
+  }
+
+  static public function create(array $params)
+  {
+    return new Department(
+      $params['id'],
+      $params['name'],
+      $params['address'],
+      $params['phone'],
+      $params['email'],
+      $params['website'],
+      $params['head_of_department']
+    );
+  }
 
   static public function all()
   {
     $conn = DB::getConn();
-    $sql = "SELECT `id`, `name`, `email` FROM `departments`";
+    $sql = "SELECT * FROM `departments`";
     $result = $conn->query($sql);
     $conn->close();
-    return $result;
+
+    return Department::resultToArray($result);
+  }
+
+  static public function resultToArray($result)
+  {
+    if ($result && $result->num_rows > 0) {
+      $items = [];
+      while ($row = $result->fetch_assoc()) {
+        $department = Department::create($row);
+        $items[] = $department;
+      }
+      return $items;
+    } else {
+      return [];
+    }
   }
 }
--- views/departments/index.php
+++ views/departments/index.php
@@ -11,25 +11,29 @@
 <body>
 
   <?php
-  if ($result && $result->num_rows > 0) {
 
-    while ($row = $result->fetch_assoc()) {
+  foreach ($result as $department) :
+
+    // if ($result && $result->num_rows > 0) {
+
+    //   while ($row = $result->fetch_assoc()) {
   ?>
-      <a href="./show.php?id=<?php echo $row['id'] ?>" style="display: block; border: 1px solid black; border-radius: 10px; padding: 15px;">
-        <h3>
-          <?php echo $row['name'] ?>
-        </h3>
-        <p>
-          <?php echo $row['email'] ?>
-        </p>
-      </a>
+    <a href="./show.php?id=<?php echo $department->id ?>" style="display: block; border: 1px solid black; border-radius: 10px; padding: 15px;">
+      <h3>
+        <?php echo $department->name ?>
+      </h3>
+      <p>
+        <?php echo $department->email ?>
+      </p>
+    </a>
   <?php
-    }
-  } else if ($result) {
-    echo 'nessun risultato';
-  } else {
-    echo 'Query error';
-  }
+  //   }
+  // } else if ($result) {
+  //   echo 'nessun risultato';
+  // } else {
+  //   echo 'Query error';
+  // }
+  endforeach;
   ?>
 
 </body>
